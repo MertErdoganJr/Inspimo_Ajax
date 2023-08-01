dc896fa3792f3326559e1a2a73c672b95eccec16
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-08-01 19:51:45 +0300

    Add project files.

---
+++ Inspimo_Ajax/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inspimo_Ajax.DAL
+{
+
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JNJNVEQ\\MERTSQL;initial Catalog=AjaxDb; integrated Security=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
